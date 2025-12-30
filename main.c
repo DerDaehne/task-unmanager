8cfb48fe00ab24491b3d27987e393447b3b74ec5
David Daehne <david.daehne@mailbox.org>
2025-12-30 17:49:25 +0100

    read system's memory info and print it line by line

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 
-int
-main() {
+#define READ_BUFFER_SIZE 256
+#define MEMINFO_FILE_PATH "/proc/meminfo"
 
-  printf("Hello World!");
+int main() {
+    char line_read[READ_BUFFER_SIZE];
+    FILE *meminfo = fopen(MEMINFO_FILE_PATH, "r");
 
-  return 0;
+    if (meminfo == NULL) {
+        printf("ERROR: can't open %s", MEMINFO_FILE_PATH);
+        return 1;
+    }
+
+
+    while (fgets(line_read, READ_BUFFER_SIZE, meminfo)) {
+        printf("Read %d Characters from meminfo:\n %s", READ_BUFFER_SIZE, line_read);
+    }
+
+    fclose(meminfo);
+    return 0;
 }
