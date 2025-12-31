3231634d9fcf2a39a9589a99df9b4405b13e3e98
David Daehne <david.daehne@mailbox.org>
2025-12-31 21:08:12 +0100

    retrive USER_HZ from Kernel

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #define READ_BUFFER_SIZE 256
 #define PROC_MEMINFO "/proc/meminfo"
@@ -8,7 +9,9 @@ int get_meminfo_measurement(char *desired_measurement, int *measurement_value, c
 
 int main() {
     int mem_total = 0;
-    char mem_unit[16];
+    char mem_unit[16] = "";
+
+    long USER_HZ = sysconf(_SC_CLK_TCK);
 
     if (get_meminfo_measurement("MemTotal:", &mem_total, mem_unit) != 0) {
         fprintf(stderr, "ERROR: could not get Memory Measurement!\n");
@@ -16,6 +19,7 @@ int main() {
     }
 
     printf("This system has: %d %s of total memory\n", mem_total, mem_unit);
+    printf("This system has a USER_HZ of: %ld Jiffies/s ", USER_HZ);
 
     return 0;
 }
@@ -40,9 +44,9 @@ int get_meminfo_measurement(char *desired_measurement, int *measurement_value, c
         return -1;
     }
 
-    char meminfo_measurement[64];
+    char meminfo_measurement[64] = "";
     int meminfo_value = 0;
-    char meminfo_unit[16];
+    char meminfo_unit[16] = "";
 
     while (fgets(line_read, READ_BUFFER_SIZE, meminfo)) {
         if (sscanf(line_read, "%63s %d %15s", meminfo_measurement,  &meminfo_value, meminfo_unit ) < 3) {
@@ -51,7 +55,7 @@ int get_meminfo_measurement(char *desired_measurement, int *measurement_value, c
 
         if (!strcmp(desired_measurement, meminfo_measurement)) {
             *measurement_value = meminfo_value;
-            strcpy(measurement_unit, meminfo_unit);
+            strncpy(measurement_unit, meminfo_unit, 16);
             fclose(meminfo);
             return 0;
         }
@@ -61,3 +65,7 @@ int get_meminfo_measurement(char *desired_measurement, int *measurement_value, c
     return 1;
 
 }
+
+// TODO: write a function which calculates the cpu usage
+// we can fetch the required data from /proc/stat, line "cpu" value fields: user(1), nice(2), system(3) and idle(4)
+//
